e584299b8e3eb3ce7f1b19febe149db4ee7278f7
Aditya Sharma <67048929+extinctsion@users.noreply.github.com>
2024-01-30 19:09:20 +0000

    completed app

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -9,7 +9,7 @@ import { EXAMPLES } from './data-examples.js';
 
 function App() {
   const [selectedTopic, setSelectedTopic] = useState();
-
+ 
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
     console.log(selectedTopic);
@@ -36,27 +36,24 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcepts
-              {...CORE_CONCEPTS[0]}
-            />
-            <CoreConcepts
-              {...CORE_CONCEPTS[1]}
-            />
-            <CoreConcepts
-              {...CORE_CONCEPTS[2]}
-            />
-            <CoreConcepts
-              {...CORE_CONCEPTS[3]}
-            />
+            {CORE_CONCEPTS.map((conceptItems) => <CoreConcepts key={conceptItems.title} {...conceptItems}/>)}
           </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
-            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+            <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect('components')}>
+              Components
+            </TabButton>
+            <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect('jsx')}>
+              JSX
+            </TabButton>
+            <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect('props')}>
+              Props
+            </TabButton>
+            <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect('state')}>
+              State
+            </TabButton>
           </menu>
           {tabContent}
         </section>
